767060092562078a89d8e151fef58d6540936edd
catoprog <@DESKTOP-GTV0AGT>
2021-10-14 13:43:43 +0300

    done

--- main.js
+++ main.js
@@ -3,38 +3,55 @@ const player1 = {
     hp: 90,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif' ,
     weapon: [
-            'sword'
-  ],
+            'sword'],
     attack: function() {
-        console.log(player1.name + ' ' + 'Fight...');
+        console.log(player1.name + 'Fight...');
     }
 }
 
 const player2 = {
     name: 'SUB-ZERO',
-    hp: 80,
+    hp: 60,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: [
-            'ice sword'
-    ],
+            'ice sword'],
     attack: function() {
-        console.log(player2.name + ' ' +'Fight...');
+        console.log(player2.name + 'Fight...');
     }
 }
 
-function createPleer (player1) {
-    const $root = document.querySelector('.root');
+function createPlayer (playerClass, player) {
+    const $player = document.createElement('div');
+    $player.classList.add(playerClass);
 
-
-    const $player1 = document.createElement('div');
-    $player1.classList.add('player1');
 
     const $progressBar = document.createElement('div');
     $progressBar.classList.add('progressbar');
+    $player.appendChild($progressBar);
 
-const $character = document.createElement('div');
-$character.classList.add('character');
+    const $character = document.createElement('div');
+    $character.classList.add('character');
+    $player.appendChild($character);
 
-const $
+    const $life = document.createElement('div');
+    $life.classList.add('life');
+    $life.style.width = player.hp + "%" ;
+    $progressBar.appendChild($life);
+    
+    const $name = document.createElement('div');
+    $name.classList.add('name');
+    $name.innerText = player.name;
+    $progressBar.appendChild($name);
 
-}
+    const $img = document.createElement('img');
+    $img.src = player.img;
+    $character.appendChild($img);
+ 
+    
+
+    const $arenas = document.querySelector('.arenas');
+    $arenas.appendChild($player);
+};
+
+createPlayer('player1', player1);
+createPlayer('player2', player2);
